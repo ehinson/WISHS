1b0e966c85698511c58052e7c73d2e16f1a0acc3
Erin Hinson <hinson.erin@gmail.com>
2014-12-27 18:46:30 -0500

    Initial commit. Add Foundation.

---
+++ app/views/post_categories/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post_category, :id, :post_id, :category_id, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MiniJournal::Application.config.secret_key_base = '7e32b4c6750f5134112e506c5c932adc2ff9ab11204703fb51362da361dd258b578e504eb1601f739fea41f3c2ac2df6be67d0f347c71016e462ce31a53482a9'
